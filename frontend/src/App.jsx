36ca7762eee06d5f483095049f40729cb5da39a3
anuragsingh8076 <anuragrajput9871595773@gmail.com>
2025-07-14 14:52:52 +0530

    Handle APIs like a pro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,42 +5,75 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
+
+ // const [products, error, loading] = customReactQuery('/api/products')
+
   const [Products, setProducts] = useState([])
   const [error, setError] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
-    (async() => {
+    const controller = new AbortController()
+    ;(async() => {
      try {
       setLoading(true)
       setError(false)
-       const response = await axios.get('/api/products')
+       const response = await axios.get('/api/products?search=' + search, {
+        signal: controller.signal
+       })
        console.log(response.data);
        setProducts(response.data);
        setLoading(false)
      } catch (error) {
+      if (axios.isCancel(error)) {
+         console.log('Request canceled', error.message);
+         return
+      }
        setError(true)
        setLoading(false)
      }
       
     })()
 
-  }, [])
 
-  if (error) {
-     return <h1>Something went wrong</h1>
-  }
+    //cleanup
+    return () => {
+      controller.abort()
+    }
 
-  if (loading) {
-    return <h1>Loading...</h1>
-  }
+  }, [search])
+
+  // if (error) {
+  //    return <h1>Something went wrong</h1>
+  // }
+
+  // if (loading) {
+  //   return <h1>Loading...</h1>
+  // }
 
   return (
     <>
       <h1>Chai aur API in react</h1>
+      <input type="text" placeholder='Search' 
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+       
+      
+      />
+
+      {loading && (<h1>Loading...</h1>)}
+      {error && (<h1>Something went wrong</h1>)}
+
       <h2>Number of Products are: {Products.length}</h2>
     </>
   )
 }
 
 export default App
+
+const customReactQuery = (urlPath) => {
+
+
+  return [Products, error, loading]
+}
